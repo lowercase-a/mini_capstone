cdcef52d190e97ca7f29887e9e2bf232d8139cbe
Brian <riceb53@gmail.com>
2019-03-06 20:22:23 -0600

    change price to decimal, change description to text

---
+++ db/migrate/20190307021316_change_columns_in_products.rb
@@ -0,0 +1,6 @@
+class ChangeColumnsInProducts < ActiveRecord::Migration[5.2]
+  def change
+    change_column :products, :description, :text
+    change_column :products, :price, :decimal, precision: 6, scale: 2
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_173037) do
+ActiveRecord::Schema.define(version: 2019_03_07_021316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
-    t.string "description"
+    t.decimal "price", precision: 6, scale: 2
+    t.text "description"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
